f45a6cfd2a0106a240e46f31fa0c46367bc3d94e
Eran Kampf <eran@ekampf.com>
2010-09-06 13:52:20 +0300

    Exception handling

--- ignition/__init__.py
+++ ignition/__init__.py
@@ -9,8 +9,10 @@ import re
 import os
 import sys
 import warnings
+import traceback
 
 import webob
+import webob.exc
 from wsgiref.handlers import CGIHandler
 
 __version__ = "0.1"
@@ -48,9 +50,7 @@ class Ignition(object):
     request = None
 
     def __init__(self, config=None, debug=False):
-        if not Ignition.instance == None :
-            raise RuntimeError, 'Only one instance of TestSingleton is allowed!'
-        Ignition.instance = self
+        self.set_instance()
 
         self.debug = debug
         self.routes = []
@@ -61,28 +61,39 @@ class Ignition(object):
     def wsgi_app(self, environ, start_response):
         environ['ignition.app'] = self
 
-        request = webob.Request(environ)
-        response = webob.Response(request=request, conditional_response=True)
-
-        if request.method not in ALLOWED_METHODS:
-            #abort(501)
-            pass
-
+        cleanup = True
         try:
-            route = self.match_route(request)
-            if not route:
-                logging.error("404")
-                pass
+            # Set the currently active request so that it can be accessed via Ignition.request
+            request = self.request_class(environ)
+            self.set_request(request)
 
+             # Make sure that the requested method is allowed in App Engine.
+            if request.method not in ALLOWED_METHODS:
+                self.halt(501) # raise webob.exec.HTTPNotImplemented()
 
-            result = route.dispatch(request)
+            matched_route = self.match_route(request)
+            if not matched_route:
+                self.halt(404)
 
-            response.content_type = 'text/plain'
-            response.body_file.write(result)
+            # TODO: Pre-dispatch filters
+
+            result = matched_route.dispatch(request)
+            response = self.make_response(request, result)
+
+            # TODO: Post-dispatch filters
+
             return response(environ, start_response)
 
+        except webob.exc.WSGIHTTPException, ex:
+            cleanup = False # TODO: should only happen on development
+            return ex(environ, start_response)
         except Exception, e:
-            logging.exception("Failure")
+            cleanup = False # TODO: should only happen on development
+            response = self.handle_exception(request, e)
+            response = self.make_response(request, response)
+        finally:
+            if cleanup:
+                self.cleanup()
 
     def match_route(self, request):
         for r in self.routes:
@@ -90,6 +101,91 @@ class Ignition(object):
                 return r
 
         return None
+
+    def make_response(self, request, result):
+        if isinstance(result, self.response_class):
+            return result
+
+        if isinstance(result, basestring):
+            return self.response_class(result)
+
+        if isinstance(result, tuple):
+            return self.response_class(*result)
+
+        if result is None:
+            raise ValueError('Handler did not return a response.')
+
+    def handle_exception(self, request, ex):
+        """Handles HTTPException or uncaught exceptions raised by the WSGI
+        application, optionally applying exception middleware.
+
+        :param request:
+            A :class:`Request` instance.
+        :param ex:
+            The catched exception.
+        :return:
+            A :class:`Response` instance, if the exception is not raised.
+        """
+        logging.exception("Unhandled exception")
+
+        # TODO: Implement
+
+        # Execute handle_exception middleware.
+        #for hook in self.middleware.get('handle_exception', []):
+        #    response = hook(e)
+        #    if response is not None:
+        #        return response
+
+
+        #if self.dev:
+        #    raise
+
+
+        lines = ''.join(traceback.format_exception(*sys.exc_info()))
+        return webob.exc.HTTPInternalServerError(detail='%s' % lines)
+
+    def halt(self, code, explanation=None, headers=None):
+        exception_classes = {
+            # Client errors
+            400 : webob.exc.HTTPBadRequest,
+            401 : webob.exc.HTTPUnauthorized,
+            402 : webob.exc.HTTPPaymentRequired,
+            403 : webob.exc.HTTPForbidden,
+            404 : webob.exc.HTTPNotFound,
+            405 : webob.exc.HTTPMethodNotAllowed,
+            406 : webob.exc.HTTPNotAcceptable,
+            407 : webob.exc.HTTPProxyAuthenticationRequired,
+            408 : webob.exc.HTTPRequestTimeout,
+            409 : webob.exc.HTTPConflict,
+            410 : webob.exc.HTTPGone,
+            411 : webob.exc.HTTPLengthRequired,
+            412 : webob.exc.HTTPPreconditionFailed,
+            413 : webob.exc.HTTPRequestEntityTooLarge,
+            414 : webob.exc.HTTPRequestURITooLong,
+            415 : webob.exc.HTTPUnsupportedMediaType,
+            416 : webob.exc.HTTPRequestRangeNotSatisfiable,
+            417 : webob.exc.HTTPExpectationFailed,
+            
+            # Server errors
+            500 : webob.exc.HTTPInternalServerError,
+            501 : webob.exc.HTTPNotImplemented,
+            502 : webob.exc.HTTPBadGateway,
+            503 : webob.exc.HTTPServiceUnavailable,
+            504 : webob.exc.HTTPGatewayTimeout,
+            505 : webob.exc.HTTPVersionNotSupported,
+        }
+
+        ex_cls = exception_classes.get(code, webob.exc.WSGIHTTPException)
+
+        ex = ex_cls()
+        if explanation:
+            ex.explanation = explanation
+
+        if headers:
+            ex.headerlist = headers
+
+        raise ex
+
 
     def route(self, url, func, method):
         """Attaches a view function to a url or a list of urls"""
@@ -105,6 +201,24 @@ class Ignition(object):
 
     def run(self):
         CGIHandler().run(self)
+
+    def set_instance(self):
+        if not Ignition.instance == None :
+            raise RuntimeError, 'Only one instance of TestSingleton is allowed!'
+        Ignition.instance = self
+
+    def set_request(self, request):
+        """Sets the currently active :class:`Request` instance.
+
+        :param request:
+            The currently active :class:`Request` instance.
+        """
+        Ignition.request = request
+
+    def cleanup(self):
+        """Cleans :class:`Ignition` variables at the end of a request."""
+        Ignition.request = None
+
 
 class Route(object):
     url_syntax = re.compile(r'''\{(\w+)(?::([^}]+))?\}''', re.VERBOSE)
@@ -206,7 +320,8 @@ def head(url=None):   return route(url, 'head')
 def put(url=None):    return route(url, 'put')
 def delete(url=None): return route(url, 'delete')
 
-
+def halt(code, explanation=None, header=None):
+    Ignition.instance.jalt(code, explanation=explanation, header=header)
 
 def make_wsgi_app(config=None, **kwargs):
     """Returns an instance of :class:'Ignition'.
